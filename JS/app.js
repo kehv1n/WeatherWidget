119188c85ce7ddd3053175ea8f6762d593ddd78b
Kevin D. Silva <kevin.dasilva1@gmail.com>
2019-02-21 15:52:53 -0500

    hit IP api and was able to determine users location using latitude and longitude. everything works ok

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,35 @@
-console.log('hellow ap.js');
+// Author: Kevin Da Silva
+// Date Created: 02/21/2019
+// Date Modified: --
+// Temperature App
+// "Created for a grade, inspired by a passion"
+
+// This APP.JS will serve as the "orchestrator" of the onsomble,
+// and this entire app. What functions will be called and when.
+
+
+// Making use of jQuerys extensive doc.ready()
+// &&  ES6 functions
+$(document).ready(() => {
+
+  var latitude,
+  longitude,
+  description,
+  weatherid,
+  temperature,
+  wind;
+
+
+  // get the location
+  var loc = GetCurrentLocation();
+
+  // use location to determine temperature
+  // GetTheTemperature(location);
+
+  // use the temperature to determine an appropriate bg
+
+  // bg = GetTheBg(temperature);
+
+
+
+});
--- JS/temperature.js
+++ JS/temperature.js
@@ -1,1 +1,23 @@
-console.log('this is temp');
+
+function GetCurrentLocation(){
+  loc = [];
+  // Finding the latitude and longitude, to be able to find the location
+  // using IP api  
+  $.getJSON("https://ipapi.co/json", (data) => {
+    // get the lat & long
+    loc.push(data.latitude, data.longitude);
+  });
+  // return the loc
+  return loc;
+}
+
+function GetTheTemperature(loc){
+   $.getJSON("https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&APPID=44bebd8c8c0a7e6f1191c819f14d195c",
+   (data) => {
+     console.log(data);
+
+   });
+
+
+
+}
