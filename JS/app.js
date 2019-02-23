db174855280c91d1933c19dbbfcdbde8ac200408
Kevin D. Silva <kevin.dasilva1@gmail.com>
2019-02-22 20:17:43 -0500

    Added getters for getting temperature from weather API and for getting background from unsplash api. All API's return data successsfully from here.

--- JS/app.js
+++ JS/app.js
@@ -15,7 +15,39 @@ $(document).ready(() => {
 
 
   // get the location
-  var location = GetCurrentLocation();
+  var location = getCurrentLocation();
+  // resolve the promise into the users current location data
+  location.then((data) => {
+  // get the temp
+    const weatherInfo = getTheTemperature(data);
+    weatherInfo.then((info) => {
+      console.log('Weather Information Below');
+      console.log(info);
+
+      // Determine which background to use according to temperature
+      // note: the api returns temp in Kelvin
+      const backgroundImageUrl = getBg(info.weather);
+      backgroundImageUrl.then((url) => {
+        console.log('the url is below');
+
+        // take this url and update the background img
+        // ** make sure to include author of pic ** 
+        console.log(url);
+
+
+
+      });
+
+
+
+
+
+
+
+    });
+   });
+
+
 
   // LOCATION keys [location.latitude location.longitude] are currently going undefined.
 
--- JS/temperature.js
+++ JS/temperature.js
@@ -1,28 +1,48 @@
 
 // ********************************************************************************
 // Name: GetCurrentLocation
-// Description: Function is used as a getter to interface with the IP API to determine users current location
-// Return Value: Location object with (2) keys for latitude and longitude
+// Description: Function is used as a getter to interface with the IP API to determine users current location.
+// Using Es7 Async Await to handle the API promise
+// Return Value: Returns a promise object that can be resolved using .then()
 // When Is Called: Upon the document.ready() callback. Only when everything on the DOM is loaded.
 // ********************************************************************************
-function GetCurrentLocation(){
-  var loc = {};
-  // Finding the latitude and longitude, to be able to find the location
-  // using IP api
-  $.getJSON("https://ipapi.co/json", (data) => {
-    // get the lat & long
-    loc.latitude = data.latitude;
-    loc.longitude = data.longitude;
-    // Get the temperatures from
-    return loc;
-  });
+async function getCurrentLocation(){
+  // hit the api, await for the response to return and save the response object from fetch()
+  const response = await fetch("https://ipapi.co/json");
+  // return data as a promise
+  return data = await response.json();
 }
 
-function GetTheTemperature(loc){
-  // console.log(loc.latitude);
-  let api_string = "https://api.openweathermap.org/data/2.5/weather?lat=" + loc.latitude + "&lon=" + loc.longitude + "&appid=059dcee9c15c93a942eb1f38b72876be";
-  console.log(api_string);
+// ********************************************************************************
+
+// ********************************************************************************
 
 
 
+async function getTheTemperature(loc){
+  const api_string = "https://api.openweathermap.org/data/2.5/weather?lat=" + loc.latitude + "&lon=" + loc.longitude + "&appid=059dcee9c15c93a942eb1f38b72876be";
+
+  const response = await fetch(api_string);
+  return data = await response.json();
+
+}
+
+// ********************************************************************************
+
+// ********************************************************************************
+
+async function getBg(weather){
+  // take the weather info desciption and use it to find an appropriate background
+  // const query = weather[0].description;
+  const query = "cloudy skies"
+  const orientation = "landscape";
+  const per_page = 5;
+
+  const api_str = `https://api.unsplash.com/search/photos/?page=1&per_page=${per_page}&query=${query}&orientation=${orientation}&client_id=33afff334fb4c4d283b8024ff44ce083c32717687eb8b764f5d816bf5f108236`;
+  const response = await fetch(api_str);
+
+  let imgUrl = await response.json();
+  imgUrl = imgUrl.results[0].links.download;
+  // return the imageUrl as a promise
+  return imgUrl;
 }
